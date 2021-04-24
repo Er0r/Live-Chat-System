cf9825fdec178da4d2b1dea80cc8e3ca4cdaf051
Er0r <fahimmaria155@gmail.com>
2021-04-25 03:11:56 +0600

    first commit"

---
+++ server.js
@@ -0,0 +1,37 @@
+
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = 3000
+
+app.set('views', path.join(__dirname, 'views')) 
+app.set('view engine', 'ejs') 
+
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+io.on('connect', socket => {
+  console.log('User Connected')
+
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User Disconnected');
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
+})
